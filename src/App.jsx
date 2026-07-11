d4ec81803fed0e2d2ad8f4cd009cc4e072dad984
Zephyr <zephyr@ZephyrMacBook-Pro.local>
2026-07-11 11:06:51 +0800

    feat: wrap App with ToastProvider

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ import ChatApp from 'components/ChatApp'
 import { HashRouter as Router } from 'react-router-dom'
 import { SocketProvider } from './context/SocketContext'
 import { MessageProvider } from './context/MessageContext'
+import { ToastProvider } from 'components/Toast'
 
 function App () {
   return (
     <Router>
-      <SocketProvider>
-        <MessageProvider>
-          <ThemeProvider theme={theme}>
-            <ChatApp />
-          </ThemeProvider>
-        </MessageProvider>
-      </SocketProvider>
+      <ToastProvider>
+        <SocketProvider>
+          <MessageProvider>
+            <ThemeProvider theme={theme}>
+              <ChatApp />
+            </ThemeProvider>
+          </MessageProvider>
+        </SocketProvider>
+      </ToastProvider>
     </Router>
   )
 }
